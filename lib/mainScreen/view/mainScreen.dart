43935a3e56a72a8756946b3d6d7b53118e50710b
kwontaebin <144872939+Kwontaebin@users.noreply.github.com>
2024-12-10 15:39:31 +0900

    관광지 데이터 값 추가(json)

--- lib/mainScreen/view/mainScreen.dart
+++ lib/mainScreen/view/mainScreen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tour_list/common/component/custom_appbar.dart';
+import 'package:flutter_tour_list/common/component/custom_elevatedButton.dart';
 import 'package:flutter_tour_list/common/component/custom_text_field.dart';
 import '../component/geoCoding.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
-  @override _MainScreenState createState() => _MainScreenState();
+  @override
+  _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
@@ -15,28 +18,30 @@ class _MainScreenState extends State<MainScreen> {
   String? _longitude;
 
   Future<void> _getCoordinates() async {
-    if (address.isEmpty) return;
+    // if (address.isEmpty) return;
 
-    try {
-      final result = await _geocodingService.fetchCoordinates(address);
-      setState(() {
-        _latitude = result['latitude'].toString();
-        _longitude = result['longitude'].toString();
 
-        print(_latitude);
-        print(_longitude);
-      });
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error: ${e.toString()}')),
-      );
-    }
+    // try {
+    //   final result = await _geocodingService.fetchCoordinates(address);
+    //   setState(() {
+    //     _latitude = result['latitude'].toString();
+    //     _longitude = result['longitude'].toString();
+    //
+    //     print(_latitude);
+    //     print(_longitude);
+    //   });
+    // } catch (e) {
+    //   print('Error: ${e.toString()}');
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Naver Geocoding Example')),
+      appBar: const CustomAppBar(
+        title: "네이버 지도",
+        showLeading: false
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -50,9 +55,10 @@ class _MainScreenState extends State<MainScreen> {
               },
             ),
             const SizedBox(height: 16),
-            ElevatedButton(
+            customElevatedButton(
+              context,
+              text: "button",
               onPressed: _getCoordinates,
-              child: const Text('Get Coordinates'),
             ),
             const SizedBox(height: 16),
             if (_latitude != null && _longitude != null) ...[
